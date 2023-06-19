cccee68afafefb759e1c4c978e09118820a6b9ac
steve-ababio <49357875+steve-ababio@users.noreply.github.com>
2023-06-19 15:32:26 +0000

    first commit

---
+++ src/components/form.jsx
@@ -0,0 +1,38 @@
+import {Component} from "react";
+import "./form.css";
+export default class ClassForm extends Component{
+    constructor(){
+        super();
+        this.state = {name:"",email:"",password:""};
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);        
+    }
+    componentDidUpdate(){
+        console.log(this.state.name)
+    }
+    handleInputChange(e){
+        const {dataset,value} = e.target;
+        this.setState({[dataset.name]:value});
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        console.log("name:",this.state.name);
+        console.log("email:",this.state.email);
+        console.log("password:",this.state.password);
+    }
+    render(){
+        return (
+            <div className="wrapper">
+                <form onSubmit={this.handleSubmit} className="form">
+                    <label htmlFor="name">Name</label>
+                    <input onChange={this.handleInputChange} data-name="name" id="name" />
+                    <label htmlFor="email">Email</label>
+                    <input onChange={this.handleInputChange} data-name="email"type="email" id="email" />
+                    <label htmlFor="password">Password</label>
+                    <input onChange={this.handleInputChange} data-name = "password" type="password" id="password" />
+                    <button onClick={this.handleSubmit} className="submitbtn">submit</button>
+                </form>
+            </div>
+        )
+    }
+}
