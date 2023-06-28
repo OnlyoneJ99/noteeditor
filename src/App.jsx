a6c901068a589f78f58c9faec259fa36d830c291
steve-ababio <49357875+steve-ababio@users.noreply.github.com>
2023-06-28 03:23:19 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { PasswordGenerator } from "./components/passwordgen/passwordgen";
+import "./App.css"
+function App() {
+ 
+
+  return (
+    <>
+      <PasswordGenerator />
+    </>
+  );
+}
+
+export default App;
